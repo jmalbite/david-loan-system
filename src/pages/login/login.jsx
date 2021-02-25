e8483dbd074661ab44ce8825e055355b52d7e128
jmalbite <albite.jm@gmail.com>
2021-02-25 23:06:31 +0800

    my first commit

---
+++ src/pages/login/login.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { TextField, Button, Paper, Grid } from '@material-ui/core';
+
+const Login = (props) => {
+  return (
+    <Grid
+      container
+      direction="row"
+      justify="center"
+      alignItems="center"
+      style={{ height: '100vh' }}
+    >
+      <Grid item xs={12} sm={6} md={4}>
+        <form className="login-form">
+          <Paper style={{ padding: 30 }}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              spacing={2}
+            >
+              <Grid container direction="column" spacing={1}>
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    name="email"
+                    placeholder="email"
+                    type="text"
+                    required
+                    fullWidth
+                  />
+                </Grid>
+
+                <Grid item xs={12}>
+                  <TextField
+                    variant="outlined"
+                    name="password"
+                    placeholder="password"
+                    type="password"
+                    required
+                    fullWidth
+                    style={{ marginBottom: 10 }}
+                  />
+                </Grid>
+              </Grid>
+
+              <Grid container direction="row" spacing={1}>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    name="login"
+                    fullWidth
+                    color="primary"
+                  >
+                    Login
+                  </Button>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    variant="contained"
+                    name="login"
+                    fullWidth
+                    color="secondary"
+                  >
+                    Login with gmail
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Paper>
+        </form>
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Login;
