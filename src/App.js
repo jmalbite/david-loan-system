534ddd97ad425239135d78ee991230ae92d6e5a8
jmalbite <albite.jm@gmail.com>
2021-02-28 21:59:53 +0800

    made some changes in dashboard page

--- src/App.js
+++ src/App.js
@@ -13,7 +13,6 @@ function App() {
         {/* <SignUp /> */}
         <DashBoard />
       </div>
-      ;
     </BrowserRouter>
   );
 }
--- src/pages/dashboard/dashboard.jsx
+++ src/pages/dashboard/dashboard.jsx
@@ -1,54 +1,92 @@
 import React from 'react';
 import AnimateNumber from 'react-animated-number';
+import moment from 'moment';
 
 import { Grid, Paper, Typography } from '@material-ui/core';
 
 const Dashboard = () => {
+  var tempDate = new Date();
   return (
     <div
       className="dashboard"
       style={{
         display: 'flex',
         justifyContent: 'center',
-        marginTop: '20px',
+        marginTop: '30px',
       }}
     >
-      <Grid container direction="column" xs sm spacing={2}>
-        <Grid item xs sm>
-          <Paper
-            elevation={2}
-            style={{ padding: 20, backgroundColor: '#EDF4E0' }}
-            square
-          >
-            <Grid container spacing={1}>
-              <Grid item xs sm direction="column">
-                <Typography variant="subtitle2" color="textPrimary">
-                  Total Balance
-                </Typography>
-                <AnimateNumber
-                  value={6900}
-                  formatValue={(value) => `₱ ${value.toFixed(2)}`}
-                  duration={400}
-                  style={{ fontSize: 15, color: '#2f2fa1' }}
-                />
+      <Grid container direction="column" spacing={2}>
+        <Grid container justify="center">
+          <Grid item xs={11} sm>
+            <Paper
+              elevation={2}
+              style={{ padding: 20, backgroundColor: '#EDF4E0' }}
+              square
+            >
+              <Grid container spacing={1}>
+                <Grid item xs sm>
+                  <Typography variant="subtitle2" color="textPrimary">
+                    Total Balance
+                  </Typography>
+                  <AnimateNumber
+                    value={6900}
+                    formatValue={(value) => `₱ ${value.toFixed(2)}`}
+                    duration={400}
+                    style={{ fontSize: 15, color: '#2f2fa1' }}
+                  />
+                </Grid>
+
+                <Grid item xs sm>
+                  <Typography variant="subtitle2" color="textPrimary">
+                    Total Paid
+                  </Typography>
+                  <AnimateNumber
+                    value={500}
+                    formatValue={(value) => `₱ ${value.toFixed(2)}`}
+                    duration={400}
+                    style={{ fontSize: 15, color: '#5eda96' }}
+                  />
+                </Grid>
+
+                <Grid item xs sm>
+                  <Typography variant="subtitle2" color="textPrimary">
+                    Total Obligation
+                  </Typography>
+                  <AnimateNumber
+                    value={7400}
+                    formatValue={(value) => `₱ ${value.toFixed(2)}`}
+                    duration={400}
+                    style={{ fontSize: 15, color: '#f64c71' }}
+                  />
+                </Grid>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+
+        <Grid item>
+          <Typography variant="h6">PAYMENT HISTORY</Typography>
+        </Grid>
+
+        <Grid container justify="center">
+          <Paper elevation={3} style={{ padding: 18 }}>
+            <Grid container spacing={3}>
+              <Grid item>
+                <Typography>{moment(tempDate).format('LL')}</Typography>
               </Grid>
 
-              <Grid item xs sm direction="column">
-                <Typography variant="subtitle2" color="textPrimary">
-                  Total Paid
-                </Typography>
+              <Grid item>
+                <Typography variant="subtitle2">amount paid</Typography>
                 <AnimateNumber
-                  value={500}
+                  value={7400}
                   formatValue={(value) => `₱ ${value.toFixed(2)}`}
                   duration={400}
                   style={{ fontSize: 15, color: '#5eda96' }}
                 />
               </Grid>
 
-              <Grid item xs sm direction="column">
-                <Typography variant="subtitle2" color="textPrimary">
-                  Total Obligation
-                </Typography>
+              <Grid item>
+                <Typography variant="subtitle2">new balance</Typography>
                 <AnimateNumber
                   value={7400}
                   formatValue={(value) => `₱ ${value.toFixed(2)}`}
@@ -57,16 +95,6 @@ const Dashboard = () => {
                 />
               </Grid>
             </Grid>
-          </Paper>
-        </Grid>
-
-        <Grid item>
-          <Typography variant="h6">PAYMENT HISTORY</Typography>
-        </Grid>
-
-        <Grid item xs sm>
-          <Paper variant="outlined" elevation={3}>
-            <Typography>yey</Typography>
           </Paper>
         </Grid>
       </Grid>
