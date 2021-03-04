668a832c54b21b760cf13dcae7d76605b41f102b
jmalbite <albite.jm@gmail.com>
2021-03-04 21:59:35 +0800

    re-save index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+//imported libraries
 import rootReducer from './store/reducer/rootReducer';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
